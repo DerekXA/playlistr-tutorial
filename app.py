8fba06a20633436b0c193c815d390dd3db81bbc4
DerekXA <dxayala03@gmail.com>
2021-11-01 15:55:29 -0700

    proc

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from pymongo.message import update
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
+
+app = Flask(__name__)
+
+def video_url_creator(id_list):
+  videos = []
+  for vid_id in id_list:
+    video = 'https://youtube.com/embed/' + vid_id
+    videos.append(video)
+  return videos
+
+
+@app.route('/')
+def playlists_index():
+  """ Show all playlists """
+  return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists/new')
+def playlists_new():
+  """ Create a new playlist """
+  playlist = []
+  return render_template('playlists_new.html', playlist=playlist, title="New Playlist")
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+  """ Submit a new playlist """
+  # Grab the video IDs and make a list from them
+  video_ids = request.form.get('video_ids').split()
+  # call our helper function to create the list of links
+  videos = video_url_creator(video_ids)
+  playlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description'),
+    'videos': videos,
+    'video_ids': video_ids
+  }
+  playlists.insert_one(playlist)
+  return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+  """ Show a single playlist """
+  playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+  return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+  """ Show the edit form for the playlist """
+  playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+  return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+  """ Submit an edited playlist """
+  video_ids = request.form.get('video_ids').split()
+  videos = video_url_creator(video_ids)
+  # create our updated playlist
+  updated_playlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description'),
+    'videos': videos,
+    'video_ids': video_ids,
+  }
+  # set the former playlist to the new one we just updated/edited
+  playlists.update_one(
+    {'_id': ObjectId(playlist_id)},
+    {'$set': updated_playlist})
+  # take us ack to the playlist's show page
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+  """ Delete one playlist """
+  playlists.delete_one({'_id': ObjectId(playlist_id)})
+  return redirect(url_for('playlists_index'))
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
