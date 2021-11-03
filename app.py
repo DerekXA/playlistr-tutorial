eedeca6e026062499f05b301afdc438d94999727
DerekXA <33045959+DerekXA@users.noreply.github.com>
2021-11-02 23:19:50 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from pymongo.message import update
+import os
+from datetime import datetime
 
-client = MongoClient()
-db = client.Playlister
+# MONGODB_URI is Config Var stored in settings: https://dashboard.heroku.com/apps/playlistr-sj/settings 
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
+# db = client.test
+
+# client = MongoClient(host=f'{host}?retryWrites=false')
+# client = MongoClient(host=host)
+db = client.get_default_database()
+# db = client.Playlister
+# one collection from the database called 'playlists'
 playlists = db.playlists
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -17,16 +27,29 @@ def video_url_creator(id_list):
   return videos
 
 
+# @app.route('/')
+# def index():
+#   return render_template('home.html', msg='Flask is cool!!')
+
+# playlists = [
+#   { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#   { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+#   { 'title': 'MC Hammer', 'description': 'Hammer Time!' }
+# ]
+
+
 @app.route('/')
 def playlists_index():
   """ Show all playlists """
   return render_template('playlists_index.html', playlists=playlists.find())
+
 
 @app.route('/playlists/new')
 def playlists_new():
   """ Create a new playlist """
   playlist = []
   return render_template('playlists_new.html', playlist=playlist, title="New Playlist")
+
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -44,17 +67,21 @@ def playlists_submit():
   playlists.insert_one(playlist)
   return redirect(url_for('playlists_index'))
 
+
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
   """ Show a single playlist """
   playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-  return render_template('playlists_show.html', playlist=playlist)
+  playlist_comments = comments.find({'playlist_id': playlist_id})
+  return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
   """ Show the edit form for the playlist """
   playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
   return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -75,6 +102,7 @@ def playlists_update(playlist_id):
   # take us ack to the playlist's show page
   return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+
 @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
   """ Delete one playlist """
@@ -82,11 +110,27 @@ def playlists_delete(playlist_id):
   return redirect(url_for('playlists_index'))
 
 
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+  """ Submit a new comment """
+  comment = {
+    'playlist_id': request.form.get('playlist_id'),
+    'title': request.form.get('title'),
+    'content': request.form.get('content'),
+  }
+  comments.insert_one(comment)
+  return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 
+# USING AN <a> TAG WITHOUT POST METHOD WORKS
+@app.route('/playlists/<playlist_id>/comments/<comment_id>/delete')
+def delete_comment(playlist_id, comment_id):
+  """ Delete a comment """
+  comments.delete_one({'_id': ObjectId(comment_id)})
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
